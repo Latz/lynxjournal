6003d783cbbaf42985050e0b23e6b438f127746e
Latz <l.schroeer@gmail.com>
2026-07-13 14:36:13 +0200

    update: plugin description to reflect self-hosted aggregation and micro-blogging

--- lynxjournal.php
+++ lynxjournal.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: LynxJournal
- * Description: Save and publish links to your blog
- * Version: 1.0.2
+ * Description: Self-hosted link aggregation and micro-blogging. Transform web references into structured blog posts.
+ * Version: 1.1.0
  * Author: Latz
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
